dc145eca3e4619cc5282f91a1fbe73ea4ee62d3f
kg <nayaguny@gmail.com>
2022-02-04 23:51:00 +0900

    dddd

--- kg/src/pagelist/Main.js
+++ kg/src/pagelist/Main.js
@@ -73,7 +73,7 @@ function ResponsiveDrawer(props) {
      
         <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus   dolor purus non
           enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
           imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
           Convallis convallis tellus id interdum velit laoreet id donec ultrices.
